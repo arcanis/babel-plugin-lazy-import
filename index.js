fa8b59eacb4d084d385bf54846a43afd6783cae2
Maël Nison <nison.mael@gmail.com>
2019-09-05 01:16:43 +0200

    Adds support for parameters

--- index.js
+++ index.js
@@ -1,108 +1,118 @@
 const {wrapInterop} = require(`@babel/helper-module-transforms`);
 const template = require(`@babel/template`).default;
 
-const FUNCTION_NODES = new Set([
-    `ClassMethod`,
-    `ObjectMethod`,
-    `FunctionDeclaration`,
-    `ArrowFunctionExpression`,
-]);
-
-const DYNAMIC_IMPORT = template(`const IMPORT_TARGET = await import(IMPORT_SOURCE)`, {
+const DYNAMIC_IMPORT = template.expression(`await import(SOURCE)`, {
     allowAwaitOutsideFunction: true,
     plugins: [`dynamicImport`],
 });
 
-const PROPERTY_ACCESS = template(`PROPERTY_SOURCE.PROPERTY_NAME`);
+const CONST_DECLARATION = template.statement(`const NAME = INIT`);
+const PROPERTY_ACCESS = template.expression(`SOURCE.NAME`);
 
 module.exports = function ({types: t}) {
     function findAsyncPath(path) {
         let candidate = null
 
-        for (let current = path.scope.path; current !== null; current = current.parentPath)
-            if (FUNCTION_NODES.has(current.node.type) && current.node.async)
+        for (let current = path.getFunctionParent(); current !== null; current = current.getFunctionParent()) 
+            if (current.node.async)
                 candidate = current;
 
         return candidate;
+    }
+
+    function isInParameterList(path) {
+        const parentFunction = path.getFunctionParent();
+
+        return !path.isDescendant(parentFunction.get(`body`));
     }
 
     let programPath;
 
     return {
         visitor: {
-            Program: {
-                enter(path) {
-                    programPath = path;
-                },
+            Program(path) {
+                programPath = path;
             },
-            ImportDeclaration: {
-                enter(path) {
-                    const allRemaps = new Map();
+            ImportDeclaration(path) {
+                const allRemaps = new Map();
 
-                    for (const specifier of path.get(`specifiers`)) {
-                        const importedIdentifierName = specifier.node.local.name;
-                        const {referencePaths} = path.scope.getBinding(importedIdentifierName);
+                for (const specifier of path.get(`specifiers`)) {
+                    const importedIdentifierName = specifier.node.local.name;
+                    const {referencePaths} = path.scope.getBinding(importedIdentifierName);
 
-                        const referencesWithTheirBlocks = referencePaths.map(referencePath => {
-                            return [referencePath, findAsyncPath(referencePath)];
-                        });
+                    const referencesWithTheirBlocks = referencePaths.map(referencePath => {
+                        return [referencePath, findAsyncPath(referencePath)];
+                    });
 
-                        // If even one of the references is used in a strictly
-                        // synchronous block, there's no point in lazy-loading
-                        // the file
-                        if (referencesWithTheirBlocks.some(([, asyncPath]) => asyncPath === null))
-                            return;
-
-                        for (const [referencePath, asyncPath] of referencesWithTheirBlocks) {
-                            let remaps = allRemaps.get(asyncPath);
-                            if (typeof remaps === `undefined`)
-                                allRemaps.set(asyncPath, remaps = []);
-
-                            remaps.push([specifier, referencePath]);
-                        }
-                    }
-
-                    if (allRemaps.size === 0)
+                    // If even one of the references is used in a strictly
+                    // synchronous block, there's no point in lazy-loading
+                    // the file
+                    if (referencesWithTheirBlocks.some(([, asyncPath]) => asyncPath === null))
                         return;
 
-                    for (const [asyncPath, remaps] of allRemaps) {
-                        if (asyncPath.node.type === `ArrowFunctionExpression` && asyncPath.node.body.type !== `BlockStatement`)
-                            asyncPath.node.body = t.blockStatement([t.returnStatement(asyncPath.node.body)]);
+                    for (const [referencePath, asyncPath] of referencesWithTheirBlocks) {
+                        let remaps = allRemaps.get(asyncPath);
+                        if (typeof remaps === `undefined`)
+                            allRemaps.set(asyncPath, remaps = []);
 
-                        const id = path.scope.generateUidIdentifierBasedOnNode(path.node.id);
+                        remaps.push([specifier, referencePath]);
+                    }
+                }
 
-                        asyncPath.get(`body`).unshiftContainer(`body`, DYNAMIC_IMPORT({
-                            IMPORT_TARGET: id,
-                            IMPORT_SOURCE: path.node.source,
-                        }));
+                if (allRemaps.size === 0)
+                    return;
 
-                        for (const [specifierPath, referencePath] of remaps) {
-                            let source;
+                for (const [asyncPath, remaps] of allRemaps) {
+                    if (asyncPath.node.type === `ArrowFunctionExpression` && asyncPath.node.body.type !== `BlockStatement`)
+                        asyncPath.node.body = t.blockStatement([t.returnStatement(asyncPath.node.body)]);
 
-                            switch (specifierPath.node.type) {
-                                case `ImportSpecifier`: {
-                                    source = PROPERTY_ACCESS({
-                                        PROPERTY_SOURCE: id,
-                                        PROPERTY_NAME: specifierPath.node.imported || t.identifier(`default`),
-                                    });
-                                } break;
-                                case `ImportDefaultSpecifier`: {
-                                    source = wrapInterop(programPath, id, "default");
-                                } break;
-                                case `ImportNamespaceSpecifier`: {
-                                    source = wrapInterop(programPath, id, "namespace");
-                                } break;
-                                default: {
-                                    throw new Error(`Unsupported import specifier type "${specifierPath.node.type}"`);
-                                } break;
-                            }
+                    const idRef = path.scope.generateUidIdentifierBasedOnNode(path.node.id);
+                    let useRef = false;
 
-                            referencePath.replaceWith(source);
+                    const dynamicImport = DYNAMIC_IMPORT({
+                        SOURCE: path.node.source,
+                    });
+
+                    for (const [specifierPath, referencePath] of remaps) {
+                        let source;
+                        if (isInParameterList(referencePath)) {
+                            source = dynamicImport;
+                        } else {
+                            source = idRef;
+                            useRef = true;
                         }
+
+                        let dereference;
+                        switch (specifierPath.node.type) {
+                            case `ImportSpecifier`: {
+                                dereference = PROPERTY_ACCESS({
+                                    SOURCE: source,
+                                    NAME: specifierPath.node.imported || t.identifier(`default`),
+                                });
+                            } break;
+                            case `ImportDefaultSpecifier`: {
+                                dereference = wrapInterop(programPath, source, "default");
+                            } break;
+                            case `ImportNamespaceSpecifier`: {
+                                dereference = wrapInterop(programPath, source, "namespace");
+                            } break;
+                            default: {
+                                throw new Error(`Unsupported import specifier type "${specifierPath.node.type}"`);
+                            } break;
+                        }
+
+                        referencePath.replaceWith(dereference);
                     }
 
-                    path.remove();
-                },
+                    if (useRef) {
+                        asyncPath.get(`body`).unshiftContainer(`body`, CONST_DECLARATION({
+                            NAME: idRef,
+                            INIT: dynamicImport,
+                        }));
+                    }
+                }
+
+                path.remove();
             },
         },
     };
